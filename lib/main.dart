99c1b982d76be5a2ecf2ffde98a2c4bb7875730f
Mandeepmgr <116657459+Mandeepmgr@users.noreply.github.com>
2023-12-03 21:04:35 +0545

    a

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:medinfo/Cart/cart_page.dart';
 import 'package:medinfo/Medicine/Detail_page.dart';
 import 'package:medinfo/Home/Home_page.dart';
+import 'package:medinfo/Medicine/Medicine_page.dart';
 import 'package:medinfo/Payment/Payment_page.dart';
 import 'package:medinfo/Profile/Profile_page.dart';
 
