8ce9eef34c707a2bc27633a716ce20aea5881180
pra5hun <summerxhunt123@gmail.com>
2023-12-10 12:34:33 +0545

    Flutter Projects

--- lib/Cart/cart_page.dart
+++ lib/Cart/cart_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:badges/badges.dart' as badges;
-import 'package:medinfo/Medicine/Detail_page.dart';
+
 
 import '../Payment/Payment_page.dart';
 
@@ -27,6 +27,22 @@ class _MyCartState extends State<MyCart> {
     'https://m.media-amazon.com/images/I/41-bx-fCENL._SY466_.jpg',
     'https://i0.wp.com/www.lifesaverpharma.com/wp-content/uploads/2021/08/doxysina.jpg?fit=600%2C399&ssl=1',
   ];
+  List<int> quantities = List.filled(6,1);
+
+
+  void increaseQuantity(int index) {
+    setState((){
+      quantities[index]++;
+    });
+  }
+
+  void decreaseQuantity(int index) {
+    if (quantities[index] > 1) {
+      setState(() {
+        quantities[index]--;
+      });
+    }
+  }
 
 
   @override
@@ -39,7 +55,7 @@ class _MyCartState extends State<MyCart> {
           Center(
             child: badges.Badge(
               badgeContent: Text('0', style: TextStyle(color: Colors.white),),
-            /*  AnimationController controller = AnimationController(duration: const Duration(milliseconds: 300),
+              /*  AnimationController controller = AnimationController(duration: const Duration(milliseconds: 300),
               vsync: this);
             animationDuration: Duration(milliseconds: 300),
             */
@@ -58,78 +74,108 @@ class _MyCartState extends State<MyCart> {
             child: ListView.builder(
                 itemCount: medicineName.length,
                 itemBuilder: (context, index){
-              return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children:[
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Image(
-                              height: 100,
-                              width: 100,
-                              image: NetworkImage(medicineImage[index].toString()),
-                            ),
-                            SizedBox(width: 10,),
-                            Expanded(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(medicineName[index].toString(),
-                                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
-                                  ),
-                                  SizedBox(height: 5,),
-                                  Text(medicineUnit[index].toString() + " "+ r"NRS "+ medicinePrice[index].toString(),
-                                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
-                                  ),
-                                  SizedBox(height:8),
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children:[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Image(
+                                height: 100,
+                                width: 100,
+                                image: NetworkImage(medicineImage[index].toString()),
+                              ),
+                              SizedBox(width: 10,),
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(medicineName[index].toString(),
+                                      style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                                    ),
+                                    SizedBox(height: 5,),
+                                    Text(medicineUnit[index].toString() + " "+ r"NRS "+ medicinePrice[index].toString(),
+                                      style: TextStyle(fontSize: 17, fontWeight: FontWeight.w500),
+                                    ),
+                                    SizedBox(height:10),
 
-
-
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(builder: (context) => PaymentPage()),
-                                      );
-                                    },
-                                    child: Align(
+                                    Align(
                                       alignment: Alignment.centerRight,
                                       child: Container(
                                         height: 35,
-                                        width: 100,
-                                        child: Center(
-                                          child: Text(
-                                            "Buy",
-                                            style: TextStyle(color: Colors.white),
-                                          ),
-                                        ),
+                                        width: 150,
                                         decoration: BoxDecoration(
                                           color: Colors.green,
-                                          borderRadius: BorderRadius.circular(15),
+                                          borderRadius: BorderRadius.circular(10),
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(4.0),
+                                          child: Row(
+                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              IconButton(
+                                                icon: Icon(Icons.remove, color: Colors.white),
+                                                onPressed: () => decreaseQuantity(index),
+                                              ),
+                                              Text(quantities[index].toString(), style: TextStyle(color: Colors.white)),
+                                              IconButton(
+                                                icon: Icon(Icons.add, color: Colors.white),
+                                                onPressed: () => increaseQuantity(index),
+                                              ),
+                                            ],
+
+                                          ),
                                         ),
                                       ),
                                     ),
-                                  ),
 
-                                ],
+
+                                    SizedBox(height:8),
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(builder: (context) => PaymentPage()),
+                                        );
+                                      },
+                                      child: Align(
+                                        alignment: Alignment.centerRight,
+                                        child: Container(
+                                          height: 35,
+                                          width: 100,
+                                          child: Center(
+                                            child: Text(
+                                              "Buy",
+                                              style: TextStyle(color: Colors.white),
+                                            ),
+                                          ),
+                                          decoration: BoxDecoration(
+                                            color: Colors.green,
+                                            borderRadius: BorderRadius.circular(15),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+
+                                  ],
+                                ),
                               ),
-                            ),
 
-                          ],
-                        )
-                      ],
+                            ],
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                );
+                  );
 
-            }
+                }
             ),
 
           )
